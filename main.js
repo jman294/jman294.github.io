948888f39022c15babff1df9c602bca9bdda0ef4
Jack DuPlessis <jduplessis294@gmail.com>
2016-12-04 14:09:31 -0500

    Added coundown timer for classic game

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 require('./styles/keyappstyle.css')
 
+import Timr from 'timrjs'
 import watch from './modules/inputeventwatcher'
 import ClassicGame from './modules/classicgame'
 
-var text = 'I am some new text'
-var asdf = new ClassicGame()
-var classicParent = asdf.create(text)
-var content = document.querySelector('#classic-wrapper')
+let timer = new Timr('4')
+let text = 'I am some new text'
+let asdf = new ClassicGame()
+let classicParent = asdf.create(text)
+let content = document.querySelector('#classic-wrapper')
 content.appendChild(classicParent)
 classicParent = document.querySelector('#classic-app')
-var button = document.querySelector('#classic-button')
+let button = document.querySelector('#classic-button')
 
 button.addEventListener('click', () => {
   startClassic()
   console.log('click')
 })
+let status = document.querySelector('#classic-status')
+status.textContent = 'To play: Type the text in the green box, backspacing if you make an error'
 const startClassic = () => {
-  document.querySelector('#classic-wrapper').removeChild(classicParent)
-  classicParent = asdf.create(text)
-  console.log(classicParent)
-  document.querySelector('#classic-wrapper').appendChild(classicParent)
+	timer.stop()
+timer = new Timr('4')
+  status = document.querySelector('#classic-status')
+  status.textContent = 'Get ready to type!'
 
-  asdf.run(text, () => {
-    console.log('done')
+  timer.start()
+  let poop = timer.ticker((formattedTime, percentDone, currentTime)=>{
+    status.textContent=currentTime
+  })
+  timer.finish((self)=>{
+    content.removeChild(classicParent)
+    classicParent = asdf.create(text)
+    console.log(classicParent)
+    content.appendChild(classicParent)
+
+    asdf.run(text, () => {
+      console.log('done')
+    })
   })
 }
